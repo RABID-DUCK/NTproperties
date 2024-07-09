d11b88090ff1fe1dc77d1e883539581b32d737eb
kayoshi03 <r0n1ronov1@gmail.com>
2024-07-09 16:06:11 +0500

    news-single

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{asset('css/uslugi.css')}}">
     <div class="container">
         <div class="uslugi">
             <h2>Услуги</h2>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/news-single.scss', 'public/css')
+    .sass('resources/sass/uslugi.scss', 'public/css')
