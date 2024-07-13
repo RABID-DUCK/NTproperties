c303a427385a9df5915290f02b02e4aac0156c57
kayoshi03 <r0n1ronov1@gmail.com>
2024-07-13 16:01:30 +0500

    guide 90/10

--- resources/views/pages/guide.blade.php
+++ resources/views/pages/guide.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-guide
+<link rel="stylesheet" href="{{asset('css/guide.css')}}">
+<div class="container">
+    <div class="guide">
+        <h1>Руководство</h1>
+        <div class="list">
+            <a href="#" class="item">
+                <img src="{{asset('img/rykovod.jpg')}}"/>
+                <h2>Владислав Рябов</h2>
+                <p>Управляющий партнер NT Properties</p>
+            </a>
+        </div>
+    </div>
+</div>
 @endsection
