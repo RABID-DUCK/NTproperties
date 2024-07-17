f7c85d62c5f7bd3fcc58c1b66b92aa168bac1df8
RABID-MOSQUITO <54327392+RABID-DUCK@users.noreply.github.com>
2024-07-18 00:02:05 +0500

    add checks

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,25 @@
 let rentBtn = document.getElementById('rentBtn');
 let saleBtn = document.getElementById('saleBtn');
 
-rentBtn.addEventListener('click', function() {
-    this.innerHTML = '<i class="fas fa-check" style="margin-right: 10px;"></i> Аренда';
-    saleBtn.innerHTML = '<i class="fas fa-times" style="margin-right: 10px;"></i> Продажа';
-    let but = document.getElementById('price_type');
-    but.setAttribute('value', '1')
-    this.classList.add('active');
-    saleBtn.classList.remove('active');
-});
+if(rentBtn  && saleBtn){
+    rentBtn.addEventListener('click', function() {
+        this.innerHTML = '<i class="fas fa-check" style="margin-right: 10px;"></i> Аренда';
+        saleBtn.innerHTML = '<i class="fas fa-times" style="margin-right: 10px;"></i> Продажа';
+        let but = document.getElementById('price_type');
+        but.setAttribute('value', '1')
+        this.classList.add('active');
+        saleBtn.classList.remove('active');
+    });
 
-saleBtn.addEventListener('click', function() {
-    this.innerHTML = '<i class="fas fa-check" style="margin-right: 10px;"></i> Продажа';
-    rentBtn.innerHTML = '<i class="fas fa-times" style="margin-right: 10px;"></i> Аренда';
-    let but = document.getElementById('price_type');
-    but.setAttribute('value', '2')
-    this.classList.add('active');
-    rentBtn.classList.remove('active');
-});
+    saleBtn.addEventListener('click', function() {
+        this.innerHTML = '<i class="fas fa-check" style="margin-right: 10px;"></i> Продажа';
+        rentBtn.innerHTML = '<i class="fas fa-times" style="margin-right: 10px;"></i> Аренда';
+        let but = document.getElementById('price_type');
+        but.setAttribute('value', '2')
+        this.classList.add('active');
+        rentBtn.classList.remove('active');
+    });
+}
 
 var inputSearch = document.getElementById("search")
 var blockSearch = document.querySelector(".search")
@@ -31,11 +33,16 @@ inputSearch.addEventListener("click", () => {
 var displayFlex = document.getElementById("fl");
 var displayGrid = document.getElementById("gr");
 var list = document.getElementById("list")
-displayFlex.addEventListener("click", () =>  {
-        list.classList.add("flex")
-})
-displayGrid.addEventListener("click", () => {
-    list.classList.remove("flex")
-})
 
+if (displayFlex){
+    displayFlex.addEventListener("click", () =>  {
+        list.classList.add("flex")
+    })
+}
+
+if(displayGrid){
+    displayGrid.addEventListener("click", () => {
+        list.classList.remove("flex")
+    })
+}
 
