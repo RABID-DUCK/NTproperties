0beb2f6a557325c18e4125f887a305dd111eff45
RABID-MOSQUITO <54327392+RABID-DUCK@users.noreply.github.com>
2024-07-18 00:28:03 +0500

    added search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+use App\Models\Objects;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function index(){
-        return view('pages.search');
+    public function index(Request $request){
+        $searchTerm = $request->input('title');
+
+        $news = News::where('title', 'LIKE', "%$searchTerm%")
+            ->orWhere('description', 'LIKE', "%$searchTerm%")
+            ->get();
+
+        $objects = Objects::where('title', 'LIKE', "%$searchTerm%")
+            ->orWhere('description', 'LIKE', "%$searchTerm%")
+            ->get();
+
+        return view('pages.search', compact('news', 'objects', 'searchTerm'));
     }
 }
